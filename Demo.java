203b330d4c1ed4426dd257e2c6fbc3178fd43928
xiaopies <xiaoshenma2016@gmail.com>
2020-10-22 02:17:45 -0400

    Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo{
-  public static void printLoop(String[]args){
+  public static void main(String[]args){
     int x = Integer.parseInt(args[0]);
     for (int i = 1; i<=x; i++){
       for (int j=x-i; j>=0; j--){
